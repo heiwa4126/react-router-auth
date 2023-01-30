1c7ed3b0eea61b36a84f469996a9bb2b529000f9
heiwa4126 <heiwa4126@gmail.com>
2023-01-30 15:09:43 +0900

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,36 +1,27 @@
-import * as path from "path";
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import rollupReplace from "@rollup/plugin-replace";
+import { ConfigEnv, defineConfig, UserConfig } from "vite";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    rollupReplace({
-      preventAssignment: true,
-      values: {
-        __DEV__: JSON.stringify(true),
-        "process.env.NODE_ENV": JSON.stringify("development"),
-      },
-    }),
-    react(),
-  ],
-  resolve: process.env.USE_SOURCE
-    ? {
-        alias: {
-          "@remix-run/router": path.resolve(
-            __dirname,
-            "../../packages/router/index.ts"
-          ),
-          "react-router": path.resolve(
-            __dirname,
-            "../../packages/react-router/index.ts"
-          ),
-          "react-router-dom": path.resolve(
-            __dirname,
-            "../../packages/react-router-dom/index.tsx"
-          ),
+export default defineConfig(({ command }: ConfigEnv) => {
+  const cfg: UserConfig = {
+    plugins: [react()],
+    build: {
+      rollupOptions: {
+        output: {
+          manualChunks: {
+            v: ["react", "react-dom", "react-router-dom"],
+          },
         },
-      }
-    : {},
+      },
+    },
+  };
+
+  if (command === "build") {
+    // when `vite build`
+    cfg.esbuild = {
+      drop: ["console", "debugger"], // https://esbuild.github.io/api/#drop
+    };
+  }
+
+  return cfg;
 });
